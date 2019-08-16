71859a3a6f79bc1ee11ba0c8509afc3584b764b6
Aamir Mirza <aamirshahed@hotmail.com>
2019-08-15 21:27:41 -0400

    Treehouse requests/code in a separate file

---
+++ treehouse.py
@@ -0,0 +1,10 @@
+import request
+
+def get_data(username):
+    """
+    Retrieve Team Treehouse data (e.g. points, badges, avatar) for a user in
+    JSON format
+    """
+    treehouse_url = "https://teamtreehouse.com/" + username + ".json"
+    response = requests.get(treehouse_url).json()
+    return response
