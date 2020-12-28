3b00032b23bf74885a49bdb481c9bba80552aa9a
Aamir Mirza <aamirshahed@hotmail.com>
2020-12-27 22:34:04 -0500

    Added single/multiple datapoint post requests to Beeminder

--- beeminder.py
+++ beeminder.py
@@ -1,6 +1,8 @@
 import time
 from dataclasses import dataclass
-from typing import Optional, Union
+from typing import Optional, Union, List
+
+import requests
 
 from params import BEEMINDER_GOAL, BEEMINDER_USERNAME, BEEMINDER_APIKEY
 
@@ -28,3 +30,23 @@ class Beeminder():
     def goal_url(self):
         return f"https://www.beeminder.com/api/v1/users/" \
                f"{self._username}/goals/{self._goal}/"
+
+    @property
+    def single_datapoint_url(self):
+        return self.goal_url + "/datapoints.json"
+
+    @property
+    def multiple_datapoints_url(self):
+        return self.goal_url + "datapoints/create_all.json"
+    
+    def send_datapoint(self, datapoint: Datapoint):
+        values = dict(auth_token = self._apikey, **datapoint.json)
+        return self._send_data(self.single_datapoint_url, values)
+
+    def send_datapoints(self, datapoints: List[Datapoint]):
+        values = dict(auth_token = self._apikey)
+        values["datapoints"] = [datapoint.json for datapoint in datapoints]
+        return self._send_data(self.multiple_datapoints_url, values)
+
+    def _send_data(self, url, data):
+        return requests.post(url, json=data)
