bd739dad750fab4af6034a70b7c0870ad69e1b5c
Aamir Mirza <aamirshahed@hotmail.com>
2020-12-28 20:26:11 -0500

    Refactored sending badges to Beeminder

--- treeminder.py
+++ treeminder.py
@@ -18,27 +18,33 @@ def validate_goal_type():
     pass
 
 
+def time_to_timestamp(time_str):
+    return calendar.timegm(time.strptime(time_str, "%Y-%m-%dT%H:%M:%S.000Z"))
+
+
+def badge_to_datapoint(badge):
+    return beeminder.Datapoint(
+        value = 1,
+        timestamp = time_to_timestamp(badge["earned_date"]),
+        comment = badge["name"],
+        request_id = str(badge["id"])
+    )
+
+
 def main():
     validate_goal_type()
     treehouse = Treehouse()
     bee = beeminder.Beeminder()
+
     if GOAL_TYPE.lower() == GoalType["BADGES"]:
-        datapoints = []
-        for badge in treehouse.badges:
-            timestamp = calendar.timegm(time.strptime(badge["earned_date"],
-                                                      "%Y-%m-%dT%H:%M:%S.000Z"))
-            datapoints.append(beeminder.Datapoint(
-                value = 1,
-                timestamp = timestamp,
-                comment = badge["name"],
-                request_id = str(badge["id"])
-            ))
+        datapoints = [badge_to_datapoint(badge) for badge in treehouse.badges]
         response = bee.send_datapoints(datapoints)
     elif GOAL_TYPE.lower() == GoalType["POINTS"]:
         datapoint = beeminder.Datapoint(value = treehouse.total_points)
         response = bee.send_datapoint(datapoint)
     else:
         raise InvalidGoalType("Goal type must be one of 'badges' or 'points'")
+
     return response
 
 
