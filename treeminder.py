ac2e6fdaf7125ea208a0f5bff84255081e3e80d0
Aamir Mirza <aamirshahed@hotmail.com>
2020-12-28 20:14:03 -0500

    Ensure that goal types are valid before making network requests.

--- treeminder.py
+++ treeminder.py
@@ -8,10 +8,21 @@ from params import GOAL_TYPE
 from treehouse import Treehouse
 
 
+class InvalidGoalType(Exception):
+    pass
+
+
+def validate_goal_type():
+    if GOAL_TYPE not in GoalType:
+        raise InvalidGoalType("Goal type must be one of 'badges' or 'points'")
+    pass
+
+
 def main():
+    validate_goal_type()
     treehouse = Treehouse()
     bee = beeminder.Beeminder()
-    if GOAL_TYPE.upper() == GoalType["BADGES"]:
+    if GOAL_TYPE.lower() == GoalType["BADGES"]:
         datapoints = []
         for badge in treehouse.badges:
             timestamp = calendar.timegm(time.strptime(badge["earned_date"],
@@ -23,9 +34,11 @@ def main():
                 request_id = str(badge["id"])
             ))
         response = bee.send_datapoints(datapoints)
-    elif GOAL_TYPE.upper() == GoalType["POINTS"]:
+    elif GOAL_TYPE.lower() == GoalType["POINTS"]:
         datapoint = beeminder.Datapoint(value = treehouse.total_points)
         response = bee.send_datapoint(datapoint)
+    else:
+        raise InvalidGoalType("Goal type must be one of 'badges' or 'points'")
     return response
 
 
