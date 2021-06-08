a89b8f280df033b5aa4bffc00ce87abf86f8affc
Aamir Mirza <spookane112@gmail.com>
2021-06-08 19:51:59 -0400

    Convert GoalType to enum

--- goaltype.py
+++ goaltype.py
@@ -1,4 +1,11 @@
-GoalType = {
-    "BADGES": "badges",
-    "POINTS": "points"
-}
+from enum import Enum, EnumMeta
+
+
+class EnumContainer(EnumMeta):
+    def __contains__(self, item):
+        return item.lower() in [member.lower() for member in self.__members__]
+
+
+class GoalType(Enum, metaclass=EnumContainer):
+    BADGES = 1
+    POINTS = 2
